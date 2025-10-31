a65f38315287325d884fa6e8b1ffd034f97c52d2
Regina Cupa <reginacupa@gmail.com>
2025-10-31 11:38:54 -0300

    Atualizações Finais

--- js/script.js
+++ js/script.js
@@ -39,6 +39,7 @@ console.log(funcionamento);
 
 funcionamento.init();
 
-fetchAnimais('../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
